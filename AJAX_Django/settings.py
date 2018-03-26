b33c189a4c7a364f33f721a40fe6af259ea0afc0
Chinmoy007 <chinmoyacharjee15@gmail.com>
2018-03-27 02:04:34 +0600

    connect MYSQL

--- AJAX_Django/settings.py
+++ AJAX_Django/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'AJAX_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ajax_django',
+        'USER': "root",
+        'PASSWORD': "chinmoy007",
     }
 }
 
